54128c4f4869a797e25ea75291e471baf0389999
ra1ne25-lab <80286530+ra1ne25-lab@users.noreply.github.com>
2022-02-07 11:34:51 +0300

    new

--- login.php
+++ login.php
@@ -5,11 +5,11 @@ session_start();
 include('db_conn.php');
 if(isset($_POST['login_btn']))
 {
+    //SELECT * FROM users WHERE username='$username' AND `password`=SHA1(CONCAT((SELECT `salt` FROM users WHERE username=$username), '$pswd'))
     $username   =   $_POST['username'];
     $name   =   $_POST['name'];
     $pswd       =   $_POST['pswd'];
-    $enc_psw    =   md5($pswd);
-    $sql        =   "SELECT * FROM users WHERE username='$username' AND `password`='$enc_psw'";
+    $sql        =   "SELECT * FROM users WHERE username='$username' AND `password`=SHA1(CONCAT((SELECT `salt` FROM users WHERE username='$username'), '$pswd'))";
     $res        =   $conn->query($sql);
     $total_rec  =   $res->rowCount();
     if($total_rec>0)
@@ -29,5 +29,15 @@ if(isset($_POST['login_btn']))
         echo"<script>alert('Произошла какая-то ошибка!')</script>";
         echo"<script>window.location.href = 'index.php';</script>";
     }
+    // $sql = "SELECT `salt` FROM users WHERE username='$username'";
+    // $res = $conn->query($sql);
+    // if ($res->rowCount() > 0) {
+    //     $row = $res->fetch();
+    //     $salt = $row['salt'];
+        
+    // } else {
+    //     echo"<script>alert('Пароль либо логин неверны')</script>";
+    //     echo"<script>window.location.href = 'index.php';</script>";
+    // }
 }
 ?>
--- registration.php
+++ registration.php
@@ -10,7 +10,7 @@ if(isset($_POST['register_btn']))
     $phno       =   $_POST['phno'];
     $pswd       =   $_POST['pswd'];
     $cnfrm_pswd =   $_POST['cnfrm_pswd'];
-    $enc_psw    =   md5($pswd);
+    // $enc_psw    =   md5($pswd);
     if($pswd==$cnfrm_pswd)
     {
         $sql        =   "SELECT * FROM users WHERE username='$username'";
@@ -21,8 +21,10 @@ if(isset($_POST['register_btn']))
             echo"<script>window.location.href = 'index.php';</script>";
             exit();    
         }
-        $sql = "INSERT INTO users (username, surname, `name`, patronymic, email, phone, `password`)
-        VALUES ('$username', '$surname', '$name', '$patronymic', '$email', '$phno', '$enc_psw')";
+        $sql = "
+            SET @salt = SUBSTRING(SHA1(RAND()), 1, 6);
+            INSERT INTO users (username, surname, `name`, patronymic, email, phone, salt, `password`)
+            VALUES ('$username', '$surname', '$name', '$patronymic', '$email', '$phno', @salt, SHA1(CONCAT(@salt, '$pswd')))";
         $conn->exec($sql);
         echo"<script>alert('Вы успешно зарегистрировались!')</script>";
         echo"<script>window.location.href = 'index.php';</script>";
